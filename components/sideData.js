1711a0b5ae689003f39c04a1a4b135bf9a57a23d
migue <miguem1994@gmail.com>
2023-07-18 23:28:03 -0300

    upload cv figma file link

--- components/sideData.js
+++ components/sideData.js
@@ -1,61 +1,66 @@
 import Image from "next/image";
-import utilStyles from "./sideData.module.css";
+import styles from "./sideData.module.css";
 
 const SideData = ({ interests, intereses, setLang, lang }) => {
+
   /* English */
   return lang == "en" ? (
-    <div className={utilStyles.sideContainer}>
-      <div className={utilStyles.containerProfilePicture}>
+    <div className={styles.sideContainer}>
+      <div className={styles.containerProfilePicture}>
         <Image
           priority
           src="/images/profilePic.jpeg"
-          className={utilStyles.profilePicture}
+          className={styles.profilePicture}
           height={300}
           width={300}
           alt={"Miguel Molina profile pic"}
         />
       </div>
-      <section className={utilStyles.heading}>
-        <h1 className={utilStyles.headingTitle}>Miguel Molina</h1>
+      <section className={styles.heading}>
+        <h1 className={styles.headingTitle}>Miguel Molina</h1>
         <p>Web Developer · Front end</p>
-        <p className={utilStyles.interestDescription}>{interests}</p>
+        <p className={styles.interestDescription}>{interests}</p>
       </section>
 
-      <section className={utilStyles.section}>
+      <section className={styles.section}>
         <p>
-          <b className={utilStyles.boldPersonalInfo}>Location</b>
+          <b className={styles.boldPersonalInfo}>Location</b>
           <br />
           Neuquen - Neuquen, Argentina
         </p>
         <p>
-          <b className={utilStyles.boldPersonalInfo}>Phone</b>
+          <b className={styles.boldPersonalInfo}>Phone</b>
           <br />
-          <a href="tel:+5493416475901" className={utilStyles.linkedInfo}>
+          <a href="tel:+5493416475901" className={styles.linkedInfo}>
             (+54) 9 341 6475901
           </a>
         </p>
         <p>
-          <b className={utilStyles.boldPersonalInfo}>Email</b>
+          <b className={styles.boldPersonalInfo}>Email</b>
           <br />
-          <a href="mailto:miguem1994@gmail.com" className={utilStyles.linkedInfo}>
+          <a href="mailto:miguem1994@gmail.com" className={styles.linkedInfo}>
             miguem1994@gmail.com
           </a>
         </p>
       </section>
 
-      <section className={utilStyles.section}>
-        <h2 className={utilStyles.titleInfo}>Languages</h2>
+      <section className={styles.section}>
+        <h2 className={styles.titleInfo}>Languages</h2>
         <p style={{cursor: "pointer"}} onClick={() => setLang("en")}>English</p>
         <p style={{cursor: "pointer"}} onClick={() => setLang("es")}>Spanish</p>
       </section>
 
-      <section className={utilStyles.section}>
-        <h2 className={utilStyles.titleInfo}>Skills</h2>
+      <section className={styles.section}>
+        <h2 className={styles.titleInfo}>Skills</h2>
         <p>ReactJS</p>
         <p>NextJS</p>
         <p>JavaScript</p>
         <p>Typescript</p>
-        <p>Figma</p>
+        <p>Figma 
+          <a className={styles.cvButton} href='https://www.figma.com/file/22dODX0ZQ8vu1JZhtCQM5y/CV?type=design&node-id=576%3A149&mode=design&t=G0jlLjheQZ8cwVvm-1' target="_blank">
+          CV
+          </a>
+        </p>
         <p>ARcore</p>
         <p>HTML / CSS</p>
         <p>Git</p>
@@ -64,63 +69,69 @@ const SideData = ({ interests, intereses, setLang, lang }) => {
         <p>Fusion360</p>
       </section>
     </div>
+
   ) : (
+
     /* Español */
-    <div className={utilStyles.sideContainer}>
-      <div className={utilStyles.containerProfilePicture}>
+    <div className={styles.sideContainer}>
+      <div className={styles.containerProfilePicture}>
         <Image
           priority
           src="/images/profilePic.jpeg"
-          className={utilStyles.profilePicture}
+          className={styles.profilePicture}
           height={300}
           width={300}
           alt={"Miguel Molina profile pic"}
         />
       </div>
 
-      <section className={utilStyles.heading}>
-        <h1 className={utilStyles.headingTitle}>Miguel Molina</h1>
+      <section className={styles.heading}>
+        <h1 className={styles.headingTitle}>Miguel Molina</h1>
         <p>Web Developer · Front end</p>
 
-        <p className={utilStyles.interestDescription}>{intereses}</p>
+        <p className={styles.interestDescription}>{intereses}</p>
       </section>
 
-      <section className={utilStyles.section}>
+      <section className={styles.section}>
         <p>
-          <b className={utilStyles.boldPersonalInfo}>Locación</b>
+          <b className={styles.boldPersonalInfo}>Locación</b>
           <br />
           Neuquen - Neuquen, Argentina
         </p>
         <p>
-          <b className={utilStyles.boldPersonalInfo}>Telefono</b>
+          <b className={styles.boldPersonalInfo}>Telefono</b>
 
           <br />
-          <a href="tel:+5493416475901" className={utilStyles.linkedInfo}>
+          <a href="tel:+5493416475901" className={styles.linkedInfo}>
             (+54) 9 341 6475901
           </a>
         </p>
         <p>
-          <b className={utilStyles.boldPersonalInfo}>Email</b>
+          <b className={styles.boldPersonalInfo}>Email</b>
           <br />
-          <a href="miguem1994@gmail.com" className={utilStyles.linkedInfo}>
+          <a href="miguem1994@gmail.com" className={styles.linkedInfo}>
             miguem1994@gmail.com
           </a>
         </p>
       </section>
 
-      <section className={utilStyles.section}>
-        <h2 className={utilStyles.titleInfo}>Lenguajes</h2>
+      <section className={styles.section}>
+        <h2 className={styles.titleInfo}>Lenguajes</h2>
         <p style={{cursor: "pointer"}} onClick={() => setLang("en")}>Ingles</p>
         <p style={{cursor: "pointer"}} onClick={() => setLang("es")}>Español</p>
       </section>
 
-      <section className={utilStyles.section}>
-        <h2 className={utilStyles.titleInfo}>Skills</h2>
+      <section className={styles.section}>
+        <h2 className={styles.titleInfo}>Skills</h2>
         <p>ReactJS</p>
         <p>NextJS</p>
         <p>JavaScript</p>
         <p>Typescript</p>
-        <p>Figma</p>
+        <p>Figma 
+          <a className={styles.cvButton} href='https://www.figma.com/file/22dODX0ZQ8vu1JZhtCQM5y/CV?type=design&node-id=576%3A149&mode=design&t=G0jlLjheQZ8cwVvm-1' target="_blank">
+            CV
+          </a>
+        </p>
         <p>ARcore</p>
         <p>HTML / CSS</p>
         <p>Git</p>
